6814b23a061b3415f88895a49b754cf29f401652
Wiktor Ravndal <w.ravndal@gmail.com>
2020-08-20 12:37:10 +0200

    Player Brain to process input

---
+++ TheGreatest/Assets/Scripts/Actor Components/PlayerBrain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Actor_Components
+{
+    public class PlayerBrain : MonoBehaviour
+    {
+        private GameControls _controls;
+        private MovementController _movementController;
+
+        private void Awake()
+        {
+            _movementController = GetComponent<MovementController>();
+            
+            _controls = new GameControls();
+            _controls.Default.Move.performed += HandleMove;
+            _controls.Default.Move.canceled += HandleMove;
+            _controls.Enable();
+        }
+
+        private void HandleMove(InputAction.CallbackContext context)
+        {
+            _movementController.MoveVector = context.ReadValue<Vector2>();
+        }
+    }
+}
