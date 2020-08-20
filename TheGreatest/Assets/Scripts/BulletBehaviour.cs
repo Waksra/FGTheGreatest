76a5787fda54aed29ac3eb806f6fabcfab41e202
juthulin <juthulin@gmail.com>
2020-08-20 13:20:00 +0200

    BulletPrefab, ObjectPooler and Background

---
+++ TheGreatest/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField] private Collider2D col;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private int bulletSpeed;
+
+    private void Update()
+    {
+        rb.velocity = transform.up * bulletSpeed;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Boundry Box")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
