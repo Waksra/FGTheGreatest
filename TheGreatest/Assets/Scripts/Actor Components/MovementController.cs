483f5f394a8a02c1c34fea7a79c6ad49853bd032
Wiktor Ravndal <w.ravndal@gmail.com>
2020-08-20 11:50:04 +0200

    Movement Controller

---
+++ TheGreatest/Assets/Scripts/Actor Components/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Actor_Components
+{
+    public class MovementController : MonoBehaviour
+    {
+        [SerializeField]
+        private float moveSpeed;
+        
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = GetComponent<Transform>();
+        }
+
+        public void Move(Vector2 direction)
+        {
+            if(direction.sqrMagnitude > 1f)
+                direction.Normalize();
+        
+            _transform.Translate(direction * moveSpeed);
+        }
+    }
+}
