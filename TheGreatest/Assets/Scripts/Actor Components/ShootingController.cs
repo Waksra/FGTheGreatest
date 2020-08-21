89083f1f600d808a3829bb6ed485683c254b4844
Wiktor FG <wiktor.ravndal@futuregames.nu>
2020-08-21 14:43:48 +0200

    Ability to switch projectile

--- TheGreatest/Assets/Scripts/Actor Components/ShootingController.cs
+++ TheGreatest/Assets/Scripts/Actor Components/ShootingController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Actor_Components
@@ -21,6 +22,23 @@ namespace Actor_Components
             projectile.transform.up = projectileOrigin.up;
             projectile.transform.position = projectileOrigin.position;
             projectile.SetActive(true);
+        }
+
+        public void ChangeProjectile(ObjectPooler.PooledObjects newProjectile, float duration)
+        {
+            StartCoroutine(ResetProjectileType(objectToFire, duration));
+            ChangeProjectile(newProjectile);
+        }
+
+        public void ChangeProjectile(ObjectPooler.PooledObjects newProjectile)
+        {
+            objectToFire = newProjectile;
+        }
+
+        private IEnumerator ResetProjectileType(ObjectPooler.PooledObjects projectile, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            ChangeProjectile(projectile);
         }
     }
 }
