aa9ea2065636660c37df1310cf1357c11db42d52
juthulin <juthulin@gmail.com>
2020-08-21 10:28:40 +0200

    Added SceneHandler

---
+++ TheGreatest/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    private int sceneIndex;
+
+    private void Awake()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void SwitchScene()
+    {
+        switch (sceneIndex)
+        {
+            case 0: // StartScene
+                SceneManager.LoadScene(1);
+                break;
+
+            case 1: // MainScene
+                SceneManager.LoadScene(2);
+                break;
+
+            case 2: // EndScene
+                SceneManager.LoadScene(1);
+                break;
+        }
+    }
+}
