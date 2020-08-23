2a92d2fe6821d795c8d63baf7359387098b05c66
juthulin <juthulin@gmail.com>
2020-08-23 20:33:55 +0200

    Final Push!

--- TheGreatest/Assets/Scripts/Actor Components/State Machine/States/BaseState.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Actor_Components.State_Machine.States
-{
-    public abstract class BaseState
-    {
-        public abstract FighterStateMachine.States Update(float delaTime);
-    }
-}
